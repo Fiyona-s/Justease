80e440de0540e8d102a799c5f1196c22169d0b19
Fiyona Saji <fiyonasaji@gmail.com>
2025-03-17 23:09:41 +0530

    Updated port settings for Render deployment

--- app.py
+++ app.py
@@ -649,4 +649,4 @@ if __name__ == '__main__':
     
     # Get the port dynamically assigned by Render, default to 10000
     port = int(os.environ.get("PORT", 10000))  
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
